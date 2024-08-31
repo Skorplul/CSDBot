a0a0a2e3c715826dfeb5f192c82e7ce28eebff40
Skorp <105490480+Skorplul@users.noreply.github.com>
2024-08-31 21:17:49 +0200

    An Logging API :O

---
+++ Log.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace JohnMaynard.API
+{
+    public class Log
+    {
+        /// <summary>
+        /// Sends a Debug level messages to the Bot console.
+        /// </summary>
+        /// <param name="message">The message to be sent.</param>
+        public void Debug(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now.ToString()}] [DEBUG]  {message}", Console.ForegroundColor = ConsoleColor.Cyan);
+        }
+
+        /// <summary>
+        /// Sends a Warn level messages to the Bot console.
+        /// </summary>
+        /// <param name="message">The message to be sent.</param>
+        public void Warn(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now.ToString()}] [WARN]  {message}", Console.ForegroundColor = ConsoleColor.Yellow);
+        }
+
+        /// <summary>
+        /// Sends an Error level messages to the Bot console.
+        /// </summary>
+        /// <param name="message">The message to be sent.</param>
+        public void Error(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now.ToString()}] [ERROR]  {message}", Console.ForegroundColor = ConsoleColor.Red);
+        }
+    }
+}
