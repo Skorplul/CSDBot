85f6d782d25eb120e0991d1585f52fe7b15da4a8
Skorplul <skorp1gaming@web.de>
2025-02-07 10:02:05 +0100

    added extra command files and 8-ball

--- Commands/InDiscord/8-ball.cs
+++ Commands/InDiscord/8-ball.cs
@@ -1,8 +1,18 @@
-using System;
+using Discord.WebSocket;
+using Log = CSDBot.API.Log;
 
 namespace CSDBot.Commands;
 
 public static class EightBall
 {
+    readonly static Random Random = new Random();
 
+    async public static void Execute(SocketSlashCommand command)
+    {
+        Log.Debug($"Command \"{command.Data.Name}\" has been executed by {command.User.GlobalName}!");
+        
+        string answer =  Config.Instance.BallAnswr[Random.Next(0, 9)];
+
+        await command.RespondAsync($"{answer}");
+    }
 }
