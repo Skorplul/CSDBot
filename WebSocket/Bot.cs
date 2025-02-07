c89d6c4f8ec7d44cd3178b2eb682d842e1bef604
Skorplul <skorp1gaming@web.de>
2025-02-07 11:56:46 +0100

    relocate an rewrite of reload

--- WebSocket/Bot.cs
+++ WebSocket/Bot.cs
@@ -1,8 +1,6 @@
 ﻿using CSDBot.Commands;
-using Discord;
-using Discord.Net;
+using CSDBot.Commands.InDiscord;
 using Discord.WebSocket;
-using Newtonsoft.Json;
 using Log = CSDBot.API.Log;
 
 namespace CSDBot
@@ -35,32 +33,13 @@ namespace CSDBot
                 }
                 else
                 {
-                    await command.RespondAsync("No Permission");
+                    await command.RespondAsync("No Permission", null, false, true);
                 }
             }
 
             if (commandName == "reload")
             {
-                // Let's do our global command
-                var globalCommand = new SlashCommandBuilder();
-                globalCommand.WithName("stop");
-                globalCommand.WithDescription("Stop da bot!");
-
-                try
-                {
-                    // With global commands we don't need the guild.
-                    await WebSocket._client.CreateGlobalApplicationCommandAsync(globalCommand.Build());
-                    // Using the ready event is a simple implementation for the sake of the example. Suitable for testing and development.
-                    // For a production bot, it is recommended to only run the CreateGlobalApplicationCommandAsync() once for each command.
-                }
-                catch (HttpException exception)
-                {
-                    // If our command was invalid, we should catch an ApplicationCommandException. This exception contains the path of the error as well as the error message. You can serialize the Error field in the exception to get a visual of where your error is.
-                    var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
-
-                    // You can send this error somewhere or just print it to the console, for this example we're just going to print it.
-                    Log.Error(json);
-                }
+                Reload.Execute();
             }
         }
     }
